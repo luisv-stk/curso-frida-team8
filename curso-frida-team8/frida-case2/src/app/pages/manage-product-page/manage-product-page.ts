42d5abc414641c936c08106c06854aa35568a978
Ana María Brazquez Villarán <ana.brazquez@softtek.com>
2025-10-28 23:58:33 +0100

    FIX Paginas de Ana en progreso: personal-area-page y manage-product-page

--- curso-frida-team8/frida-case2/src/app/pages/manage-product-page/manage-product-page.ts
+++ curso-frida-team8/frida-case2/src/app/pages/manage-product-page/manage-product-page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ImageUploaderComponent } from '../../components/image-uploader/image-uploader.component'; 
+import { CartComponent } from '../../components/cart/cart.component'; 
 /**
  * Standalone component para la página de gestión de productos.
  */
 @Component({
   selector: 'app-manage-product-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CartComponent, ImageUploaderComponent],
   templateUrl: './manage-product-page.html',
   styleUrls: ['./manage-product-page.css']
 })
--- curso-frida-team8/frida-case2/src/app/pages/personal-area-page/personal-area-page.ts
+++ curso-frida-team8/frida-case2/src/app/pages/personal-area-page/personal-area-page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from '../../components/table/table.component'; 
 import { CartComponent } from '../../components/cart/cart.component'; 
-import { ImageUploaderComponent } from '../../components/image-uploader/image-uploader.component'; 
+
 
 /**
  * Página de área personal que incluye la tabla usando TableComponent.
@@ -10,7 +10,7 @@ import { ImageUploaderComponent } from '../../components/image-uploader/image-up
 @Component({
   selector: 'app-personal-area-page',
   standalone: true,
-  imports: [CommonModule, TableComponent, CartComponent, ImageUploaderComponent], // TableComponent incluido
+  imports: [CommonModule, TableComponent, CartComponent], // TableComponent incluido
   templateUrl: './personal-area-page.html',
   styleUrls: ['./personal-area-page.css']
 })
